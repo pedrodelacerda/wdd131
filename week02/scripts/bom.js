56c240d36261055956f57d5d4e0b939d2c8ca688
pedrodelacerda <pedrohdelacerda@gmail.com>
2026-05-14 19:50:28 -0300

    corrections performed in the activitie

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,22 @@
 
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-li.append(li);
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function(){
+    if (input.value != ""){
+        const li = document.createElement("li");
+        li.textContent = input.value;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        li.append(deleteBtn);
+        list.append(li);
+        deleteBtn.addEventListener("click", function(){
+            list.removeChild(li);
+            input.focus();
+        }) 
+        input.value = "";
+        input.focus();
+    }
+});
+
